1eaacb9f929876ad6fe76c3166104bc919d8f92c
Ras azzab <ras.dinho7@gmail.com>
2020-02-25 20:37:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
 end
 
 def find_max_value(array)
@@ -12,7 +12,7 @@ def find_min_value(array)
 
 while counter  < array.length do
   if array[counter] < ney
-  # puts ney
+  
     ney = array[counter]
 end
 counter +=1 
