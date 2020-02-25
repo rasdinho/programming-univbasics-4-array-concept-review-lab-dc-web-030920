b23aff4b1031447518ecffc4f8d862fccb89fa7c
Ras azzab <ras.dinho7@gmail.com>
2020-02-25 20:58:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   
   array.length.times do |counter|
-     array[counter]=== value_to_find
+    if  array[counter]=== value_to_find
+    value_to_find
      
 end
 nil 
