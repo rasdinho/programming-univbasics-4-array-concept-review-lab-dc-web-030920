91ca1404c73f3f284991f72327c781eae8b7b156
Ras azzab <ras.dinho7@gmail.com>
2020-02-25 20:26:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,11 +12,10 @@ def find_min_value(array)
   
 while counter  < array.length do
   if array[counter] < ney[counter]
-    #if counter = 3 and array[3] = 2 while ney[3] = 5
-    #can you replace ney's value with array?'s value
+   
     ney = array[counter]
-counter +=1 
 end
+counter +=1 
 end
 ney
 end
