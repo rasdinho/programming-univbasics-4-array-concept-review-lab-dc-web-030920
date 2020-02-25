93980464294422e39ace2d6134b7ee77108831a0
Ras azzab <ras.dinho7@gmail.com>
2020-02-25 20:44:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,6 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |conter|
+  
+  array.length.times do |counter|
     puts value_to_find # same as putting "hello world" for length of array
 end
 value_to_find
